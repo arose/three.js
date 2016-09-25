6d62c64fd78cf5b6bf6a67433d5f531349fa6e93
arose <alexander.rose@weirdbyte.de>
2016-09-24 17:23:15 -0700

    updating interactive_instances_gpu example

--- src/renderers/WebGLRenderer.js
+++ src/renderers/WebGLRenderer.js
@@ -1507,7 +1507,7 @@ function WebGLRenderer( parameters ) {
 			object.modelViewMatrix.multiplyMatrices( camera.matrixWorldInverse, object.matrixWorld );
 			object.normalMatrix.getNormalMatrix( object.modelViewMatrix );
 
-			object.onBeforeRender( _this, scene, camera, geometry, material, group );
+			object.onBeforeRender( _gl, _this, scene, camera, geometry, material, object, group );
 
 			if ( object.isImmediateRenderObject ) {
 
@@ -1529,7 +1529,7 @@ function WebGLRenderer( parameters ) {
 
 			}
 
-			object.onAfterRender( _this, scene, camera, geometry, material, group );
+			object.onAfterRender( _gl, _this, scene, camera, geometry, material, object, group );
 
 
 		}
